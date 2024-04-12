638d0e7bf436c1c626901a16fe3d59e6de822d1b
egor <seniorpetrenko@gmail.com>
2024-04-12 12:03:55 +0300

    Добавление таблицы artists в код

--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -104,7 +104,7 @@ public class MainActivity extends AppCompatActivity implements TransactionEvents
         new Thread(() -> {
             try {
                 HttpURLConnection uc = (HttpURLConnection)
-                        (new URL("http://192.168.70.150:8081/api/v1/title").openConnection());
+                        (new URL("http://192.168.55.220:8081/api/v1/title").openConnection());
                 InputStream inputStream = uc.getInputStream();
                 String html = IOUtils.toString(inputStream);
                 String title = getPageTitle(html);
